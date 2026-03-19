87c389ca7aa8b160670751f58c1d3152d714f40b
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-03-19 18:19:23 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+    int a, b;
+    char c;
 
+    scanf("%d %d %c", &a, &b, &c);
+    
+    if (c == '+') printf("%d\n", a + b);
+    else if (c == '-') printf("%d\n", a - b);
+    else if (c == '*') printf("%d\n", a * b);
+    else if (c == '/') printf("%d\n", a / b);
+    
     return 0;
 }
-
