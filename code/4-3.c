e2a9fcaf1673a5738115698d7557a8dd0119bd5a
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-03-19 18:19:54 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,21 @@
 
 int main()
 {
-
+    int a;
+    scanf("%d", &a);
+    for (int i = 2; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            printf("false");
+            break;
+        }
+        else
+        {
+            printf("true");
+            break;
+        }
+    }
+    
     return 0;
 }
-
